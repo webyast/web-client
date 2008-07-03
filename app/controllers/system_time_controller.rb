0e4f05d32483c9c38861ebc56e975e86c97f88d7
Stefan Schubert <schubi@suse.de>
2008-07-03 19:02:59 +0200

    first try for showing the time data

--- app/controllers/system_time_controller.rb
+++ app/controllers/system_time_controller.rb
@@ -1,13 +1,26 @@
 require 'systemtime'
 
 class SystemTimeController < ApplicationController
-
+  layout 'main'
   def index
-    #t = SystemTime.find(:all, :from => '/system/time', :element_name => 'system-time')
-    #render :xml => t.to_xml
+    t = SystemTime.find(:one, :from => '/system/time', :element_name => 'system-time')
+    @systemtime = t.systemtime
+    @timezone = t.timezone
+    if t.is_utc=true
+      @is_utc = "checked" 
+    else 
+      @is_utc = ""
+    end
+  end
 
-    #servers = Services::Ntp::Config::Server.find(:all)
-
+  def commit_time
+#    t = SystemTime.find(:one, :from => '/system/time', :element_name =>
+#    'system-time')
+ render :text =>  request.path_parameters.to_s
+#    t.systemtime = params[systemtime]
+#    t.timezone = params[timezone]
+#    t.save
+#    redirect_to :action => :index
   end
 
 end
