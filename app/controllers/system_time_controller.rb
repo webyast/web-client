55af256fe3e80771119d86fe6a2702e46aad9b58
Stefan Schubert <schubi@waerden.suse.de>
2008-10-20 16:25:13 +0200

    adapt to systemtime

--- app/controllers/system_time_controller.rb
+++ app/controllers/system_time_controller.rb
@@ -2,28 +2,40 @@ class SystemTimeController < ApplicationController
   before_filter :login_required
   layout 'main'
   def index
-    t = SystemTime.find(:one, :from => '/systemtime.xml')
-    @systemtime = t.currenttime
-    @timezone = t.timezone
-    if t.is_utc == true
+    @systemtime = SystemTime.find(:one, :from => '/systemtime.xml')
+    if params[:last_error] && params[:last_error] != 0
+       @systemtime.error_id = params[:last_error]
+       if params[:last_error_string]
+          @systemtime.error_string = params[:last_error_string]
+       end
+    end
+    if @systemtime.is_utc == true
       @is_utc = "checked" 
     else 
       @is_utc = ""
     end
+    @timeString = @systemtime.currenttime.hour.to_s
+    @timeString << ":" 
+    @timeString << @systemtime.currenttime.min.to_s
   end
 
   def commit_time
     t = SystemTime.find(:one, :from => '/systemtime.xml')
-    t.id = "systemtime"
-    t.systemtime = params[:systemtime]
     t.timezone = params[:timezone]
     if params[:utc] == "true"
        t.is_utc = true
     else
        t.is_utc = false
     end
+    timeArray = params[:currenttime].split ":"
+    t.currenttime = DateTime.civil(params[:year].to_i, params[:month].to_i, 
+                                   params[:day].to_i, timeArray[0].to_i, timeArray[1].to_i)
     t.save
-    redirect_to :action => :index
+    if t.error_id != 0
+       redirect_to :action => :index, :last_error_string =>t.error_string, :last_error =>t.error_id 
+    else
+       redirect_to :action => :index 
+    end
   end
 
 end
