3ef6ffa3d5532540ae1e1a6d80ce85a85e60b49f
Stefan Schubert <schubi@waerden.suse.de>
2008-10-29 15:00:20 +0100

    running services

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -9,20 +9,32 @@ class CommandsController < ApplicationController
   def index
     id = params[:service_id]
     logger.debug "services/show #{id}"
-    @service = Service.find(id, :from => '/services.xml')
+    @service = Service.find(id)
     @commands = []
     @commands = @service.commands.split(",") unless @service==nil
+    if params[:last_error] && params[:last_error] != 0
+       @service.error_id = params[:last_error]
+       if params[:last_error_string]
+          @service.error_string = params[:last_error_string]
+       end
+    end
     render
   end
 
   def update
-    id = params[:service_id]
-    logger.debug "services/show #{id}"
-    @service = Service.find(id, :from => '/services.xml')
-    @commands = []
-    @commands = @service.commands.split(",") unless @service==nil
-    flash[:notice] = 'Command has been run successfully'
-    redirect_to :back, :action => "show" 
+    @service = Service.find(params[:service_id])
+    @service.id = @service.link
+    commandId = "commands/"
+    commandId << params[:id]
+    logger.debug "calling #{commandId} with service #{@service.inspect}"
+    response = @service.put(commandId)
+    retService = Hash.from_xml(response.body)
+    logger.debug "returns #{retService["service"].inspect}"  
+    if retService["service"]["error_id"] != 0
+       redirect_to :back, :action => "show" , :last_error_string =>retService["service"]["error_string"], :last_error =>retService["service"]["error_id"]
+    else
+       redirect_to :back, :action => "show"
+    end
   end
 
 end
