c145ce854b0be55ba033c06afcc5c481d7741b87
Stefan Schubert <schubi@waerden.suse.de>
2009-01-09 13:33:28 +0100

    bugfixes

--- webclient/app/controllers/permission_controller.rb
+++ webclient/app/controllers/permission_controller.rb
@@ -1,26 +1,39 @@
 class PermissionController < ApplicationController
   layout "main"
 
-
   # Checks the tree if there is a node which is set to the value of "grant"
-  def findGrant (tree, grant)
+  # and check if the subtree has to be shown
+  def showSubtree (tree, grant)
      return false if !tree
-     found = false
+     show = false
      tree.each do |key, branches|
-	if (key == :grant &&
-           branches == grant )
-           found = true
-           break
+	if (key == :grant)
+           if branches == true
+              if (grant == true) #the complete subtree is granted
+                 show = true
+              else
+                 show = false #it make no sense anymore cause the complete subtree is granted
+              end
+              break # do not calculate the subtrees
+           end
+           if (tree.size <= 2 &&
+               branches == grant)
+              #last branch reached, so take that result
+              show = true
+              break
+           end
         else
-           if branches.is_a? Hash
-              found = findGrant( branches, grant )
-              if found == true
-                 break
+           if (branches.is_a?(Hash) &&
+               branches.size > 0)
+              show = showSubtree( branches, grant )
+              if (show==true &&
+                  grant==true)
+                 break #show it in each case
               end
            end
         end
      end
-     return found
+     return show
   end
   
   def constructPermissionTree()
@@ -38,7 +51,7 @@ class PermissionController < ApplicationController
   def buildJavaVariable( tree, level, grant, takeAll, user, javaString )
      tree.each do |key, branch|
         if branch.is_a? Hash
-           if (findGrant(branch, grant) || #there is at least one item set to "grant"
+           if (showSubtree(branch, grant) || #there is at least one item set to "grant"
                takeAll)                   #or the rest should be simply taken
               javaString += "#{level}, { label: \"#{key}\", href:\""
               if branch.has_key?(:path)
@@ -50,6 +63,7 @@ class PermissionController < ApplicationController
               end
               javaString += "\" },\n"
               #taking the subtrees too
+              nextTakeAll = takeAll
               if (branch.has_key?(:grant) &&
                   grant &&
                   branch[:grant] == true)
@@ -66,19 +80,18 @@ class PermissionController < ApplicationController
   def search
     path = "/users/#{params[:user].rstrip}/permissions.xml"
     @permissions = Permission.find(:all, :from => path)
-    logger.debug "permissions of user #{params[:user].rstrip}: #{@permissions.inspect}"
+#    logger.debug "permissions of user #{params[:user].rstrip}: #{@permissions.inspect}"
     @permissionTree = Hash.new{ |h,k| h[k] = Hash.new &h.default_proc }
     constructPermissionTree()
     logger.debug "Complete Tree: #{@permissionTree.to_xml}"
 
-
     @grant_data = buildJavaVariable( @permissionTree, 0, true, false, params[:user].rstrip, "var grant_data = \n [" )    
     @grant_data += "];"
-    logger.debug "Grant Tree: #{@grant_data}"
+#    logger.debug "Grant Tree: #{@grant_data}"
 
     @revoke_data = buildJavaVariable( @permissionTree, 0, false, false, params[:user].rstrip, "var revoke_data = \n [" )    
     @revoke_data += "];"
-    logger.debug "Revoke Tree: #{@revoke_data}"
+#    logger.debug "Revoke Tree: #{@revoke_data}"
 
     render :action => "index" 
   end
