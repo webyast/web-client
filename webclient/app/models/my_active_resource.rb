696f408b01140e24c30b6d6da4e7d46f8b757771
Stefan Schubert <schubi@suse.de>
2009-01-29 15:41:11 +0100

    added to_xml which is not included in version 2_1

--- webclient/app/models/my_active_resource.rb
+++ webclient/app/models/my_active_resource.rb
@@ -1,2 +1,7 @@
 class MyActiveResource < ActiveResource::Base
+
+#This is not defined in AcitveResource 2_1 but in 2_0 and 2_2
+   def to_xml(options={})
+      attributes.to_xml({:root => self.class.element_name}.merge(options))
+    end
 end
