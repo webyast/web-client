212d5309235273eac8bb9e7b6186fea7f594342e
Stefan Schubert <schubi@suse.de>
2009-01-30 15:21:24 +0100

    checking if to_xml is defined in AcitiveResource

--- webclient/app/models/my_active_resource.rb
+++ webclient/app/models/my_active_resource.rb
@@ -1,7 +1,10 @@
 class MyActiveResource < ActiveResource::Base
 
 #This is not defined in AcitveResource 2_1 but in 2_0 and 2_2
+unless ActiveResource::Base.instance_methods.include? 'to_xml'
    def to_xml(options={})
       attributes.to_xml({:root => self.class.element_name}.merge(options))
     end
+end
+
 end
