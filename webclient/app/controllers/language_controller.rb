059ce1a131357623a199bc04abc2a146699b4fc1
Stefan Schubert <schubi@suse.de>
2009-02-17 17:26:03 +0100

    improved API for language interface

--- webclient/app/controllers/language_controller.rb
+++ webclient/app/controllers/language_controller.rb
@@ -1,56 +1,51 @@
 class LanguageController < ApplicationController
   before_filter :login_required
   layout 'main'
+
   def index
     set_permissions(controller_name)
-
     @language = Language.find(:one, :from => '/language.xml')
-    @valid = []
     @second_languages = {}
-    split_second_languages = []
-    if @language.second_languages
-       split_second_languages = @language.second_languages.split(",")
-    end
-    @valid = @language.available.split("\n")
-    @valid::each do |l|   
-       dummy = l.split(" ")
-       if dummy.size>0 && dummy[0]==@language.first_language
-          @language.first_language = l
+    @valid = []
+
+    @language.available.each do  |avail|
+       @valid << avail.name
+       if avail.id.size>0 && avail.id==@language.first_language
+          @language.first_language = avail.name
        end
        checked = ""
-       split_second_languages::each do |s|
-          if dummy.size>0 && dummy[0]==s
+       @language.second_languages::each do |s|
+          if avail.id.size>0 && avail.id==s.id
              checked = "checked"
           end
        end
-       @second_languages[l] = checked
+       @second_languages[avail.name.tr_s(" ", "_")] = checked
     end
   end
 
   def commit_language
-    lang = Language.find(:one, :from => '/language.xml')
-    dummy = params[:first_language].split(" ")
-    lang.first_language = dummy[0] #take index only
-    lang.available = "" #not needed anymore
-    lang.second_languages = ""
-    counter = 0
-    params::each do |name,value|   
+     lang = Language.find(:one, :from => '/language.xml')
+     hash_avail = {}
+     lang.available.each do  |avail|
+        hash_avail[avail.name.tr_s(" ", "_")] = avail.id
+     end
+
+     lang.first_language = hash_avail[params[:first_language].tr_s(" ", "_")]
+     lang.available = [] #not needed anymore
+     lang.second_languages = []
+     params::each do |name,value|   
        if value=="LanguageSet"
-          if counter==0
-             lang.second_languages = name
-          else
-             lang.second_languages = lang.second_languages + "," + name
-          end
-          counter = counter+1
+          l = Language::SecondLanguage.new( :id => hash_avail[name] )
+          lang.second_languages << l
        end
-    end
-    lang.save
-    if lang.error_id != 0
-       flash[:error] = lang.error_string
-    else
-       flash[:notice] = "Settings have been written."
-    end       
-    redirect_to :action => :index 
+     end
+     lang.save
+     if lang.error_id != 0
+        flash[:error] = lang.error_string
+     else
+        flash[:notice] = "Settings have been written."
+     end       
+     redirect_to :action => :index 
   end
 
 end
