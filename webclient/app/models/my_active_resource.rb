0e358725ccd4bb066137fea541e11e70bfa792ba
Stefan Schubert <schubi@suse.de>
2009-04-02 13:05:58 +0200

    added version handling in the REST protocol

--- webclient/app/models/my_active_resource.rb
+++ webclient/app/models/my_active_resource.rb
@@ -1,5 +1,11 @@
 class MyActiveResource < ActiveResource::Base
 
+#
+# Set the version of the used REST interface.
+# Be sure to restart your server when you modify this file.
+#
+self.prefix = "/yast/v1/"
+
 #This is not defined in AcitveResource 2_1 but in 2_0 and 2_2
 unless ActiveResource::Base.instance_methods.include? 'to_xml'
    def to_xml(options={})
