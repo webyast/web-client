96e1bbb8cd3749bd70e89ff83f1c17998dfb808c
Duncan Mac-Vicar P <dmacvicar@suse.de>
2009-04-23 14:59:38 +0200

    no prefix now

--- webclient/app/models/my_active_resource.rb
+++ webclient/app/models/my_active_resource.rb
@@ -4,7 +4,6 @@ class MyActiveResource < ActiveResource::Base
 # Set the version of the used REST interface.
 # Be sure to restart your server when you modify this file.
 #
-self.prefix = "/yast/v1/"
 
 #This is not defined in AcitveResource 2_1 but in 2_0 and 2_2
 unless ActiveResource::Base.instance_methods.include? 'to_xml'
