68263ca75d3e69442395f30c37a2e22dbcfae62f
bgeuken <bgeuken@suse.de>
2009-05-15 10:18:27 +0200

    security module: added before filter to controller

--- plugins/securities/app/controllers/securities_controller.rb
+++ plugins/securities/app/controllers/securities_controller.rb
@@ -2,23 +2,26 @@ require 'yast/service_resource'
 
 
 class SecuritiesController < ApplicationController
-  before_filter :login_required
+  before_filter :login_required, :prepare
 
   # Initialize GetText and Content-Type.
   init_gettext "yast_webclient_securities"  # textdomain, options(:charset, :content_type)
+
+  private
+
+  def prepare
+    set_permissions(controller_name)
+    @client = YaST::ServiceResource.proxy_for('org.opensuse.yast.system.security')
+    @permissions = @client.permissions
+
+    @write_permission = "disabled" unless @permissions[:write]
+  end
 
   public
 
   # POST /security
   # POST /security.xml
   def create
-    set_permissions(controller_name)
-
-    @client = YaST::ServiceResource.proxy_for('org.opensuse.yast.system.security')
-    @permissions = @client.permissions
-
-    @write_permission = "disabled" unless @permissions[:write]
-
     @security = @client.find(:one, :from => '/security.xml')
 
     @firewall_after_startup = "checked" if @security.firewall_after_startup
@@ -32,17 +35,13 @@ class SecuritiesController < ApplicationController
   end
 
   def update
-    set_permissions(controller_name)
-    @client = YaST::ServiceResource.proxy_for('org.opensuse.yast.system.security')
-    @permissions = @client.permissions
-
     s = @client.find()
     s.firewall_after_startup = params[:firewall_after_startup].eql?("true")
     s.firewall = params[:firewall].eql?("true")
     s.ssh = params[:ssh].eql?("true")
-    s.save()
+    s.save() # send to rest-service
 
-    @write_permission = "disabled" unless @permissions[:write]
+    # prepare for view
     @firewall_after_startup = "checked" if s.firewall_after_startup
     @firewall = "checked" if s.firewall
     @ssh = "checked" if s.ssh
@@ -52,7 +51,7 @@ class SecuritiesController < ApplicationController
       redirect_to :action => :index  #???
 #        redirect_to project_url(@security)
     end
-      flash[:notice] = _('Settings have been written.')
+    flash[:notice] = _('Settings have been written.')
   end
 end
 
