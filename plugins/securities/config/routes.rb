ec2df4561f23f0227987c6f77d25d6a80613dddd
bgeuken <bgeuken@suse.de>
2009-05-15 11:19:26 +0200

    security controller: modified routes

--- plugins/securities/config/routes.rb
+++ plugins/securities/config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect "/security", :controller => 'securities', :action => 'index'
-  map.connect "/security/update", :controller => 'securities', :action => 'commit'
+  map.connect "/security", :controller => 'securities', :action => 'create'
+  map.connect "/security/show", :controller => 'securities', :action => 'show'
+  map.connect "/security/update", :controller => 'securities', :action => 'update'
 
 end
 
