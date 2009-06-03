04184278680b2d491e4521e745cbc9c6dd43d1d1
Ladislav Slezak <lslezak@novell.com>
2009-06-03 14:53:53 +0200

    view helpers separated to extra files
    the view helpers from application_helper.rb have been
    split to separte files under view_helpers subdirectory

---
+++ webclient/app/helpers/view_helpers/label.rb
@@ -0,0 +1,16 @@
+
+module Label
+    include GetText
+
+    def Label.add
+	return _("Add")
+    end
+
+    def Label.edit
+	return _("Edit")
+    end
+
+    def Label.delete
+	return _("Delete")
+    end
+end
