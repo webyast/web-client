07fe5a7efcd353843d4d44af5560ac14e0c59c0b
Bjoern Geuken <bgeuken@suse.de>
2009-06-15 15:04:53 +0200

    patch_updates module: added checkboxes to view, enables module to update several patches together

--- plugins/patch_updates/app/controllers/patch_updates_controller.rb
+++ plugins/patch_updates/app/controllers/patch_updates_controller.rb
@@ -4,7 +4,7 @@ class PatchUpdatesController < ApplicationController
 
   before_filter :login_required
   layout 'main'
-  
+
   # GET /patch_updates
   # GET /patch_updates.xml
   def index
@@ -25,9 +25,43 @@ class PatchUpdatesController < ApplicationController
       format.json { render :json => @patch_updates.to_json }
     end
   end
-  
+
   # POST /patch_updates/1
   # POST /patch_updates/1.xml
+
+  def install
+    proxy = YaST::ServiceResource.proxy_for('org.opensuse.yast.system.patches')
+    ok = true
+    update_array = []
+
+    params.each_pair{ |key, value|
+      if key.include?("patch_")
+        update_array << value
+      end
+    }
+    update_array.each { |patch_id|
+    begin
+      update = proxy.find(patch_id)
+      rescue ActiveResource::ClientError => e
+        flash[:error] = YaST::ServiceResource.error(e)
+        ok = false
+    end
+
+    if ok
+      begin
+        ok = update.save
+        logger.debug "updated #{patch_id}"
+        rescue ActiveResource::ClientError => e
+          flash[:error] = YaST::ServiceResource.error(e)
+          ok = false
+      end
+      flash[:notice] = _("Patches have been installed.") if ok
+    end
+    }
+    redirect_to({:controller=>"patch_updates", :action=>"index"})
+  end
+=begin
+
   def install
     proxy = YaST::ServiceResource.proxy_for('org.opensuse.yast.system.patches')
     ok = true
@@ -49,5 +83,5 @@ class PatchUpdatesController < ApplicationController
     end
     redirect_to({:controller=>"patch_updates", :action=>"index"})
   end
-
+=end
 end
