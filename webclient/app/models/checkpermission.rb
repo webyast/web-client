1eb38ffecc9301729b2572b4795f7ac71f625b18
Stefan Schubert <schubi@suse.de>
2009-06-30 16:17:59 +0200

    cleanup

--- webclient/app/models/checkpermission.rb
+++
@@ -1,6 +0,0 @@
-require 'yast/service_resource'
-
-class Checkpermission < YaST::ServiceResource::Base
-    self.collection_name = "checkpermission"
-    self.element_name = "hash"
-end
--- webclient/app/models/yast.rb
+++
@@ -1,6 +0,0 @@
-require 'yast/service_resource'
-
-class Yast < YaST::ServiceResource::Base
-  self.collection_name = "yast"
-  attr_accessor :visible_name
-end
