7054c5974330b17a4b4a2e8cd0becaf5456a99ed
Bjoern Geuken <bgeuken@suse.de>
2009-07-01 10:29:53 +0200

    patches module: * addded patch selection to the view (javascript),                 * removed show.rhtml

--- plugins/patch_updates/app/controllers/patch_updates_controller.rb
+++ plugins/patch_updates/app/controllers/patch_updates_controller.rb
@@ -8,21 +8,13 @@ class PatchUpdatesController < ApplicationController
   # GET /patch_updates
   # GET /patch_updates.xml
   def index
-  end
-
-  def list
     @proxy = YaST::ServiceResource.proxy_for('org.opensuse.yast.system.patches')
     begin
       @patch_updates = @proxy.find(:all) || []
       rescue ActiveResource::ClientError => e
         flash[:error] = YaST::ServiceResource.error(e)
     end
-    respond_to do |format|
-      format.html { render :partial => 'patches' }
-      format.js { render :partial => 'patches' }
-      format.xml  { render :xml => @patch_updates }
-      format.json { render :json => @patch_updates.to_json }
-    end
+    logger.debug @patch_updates.nil?
   end
 
   # POST /patch_updates/1
@@ -33,54 +25,31 @@ class PatchUpdatesController < ApplicationController
     ok = true
     update_array = []
 
-    params.each_pair{ |key, value|
-      if key.include?("patch_")
+    #search for patches and collect the ids
+    params.each { |key, value|
+      if key =~ /\D*_\d/
         update_array << value
       end
     }
     update_array.each { |patch_id|
-    begin
-      update = proxy.find(patch_id)
-      rescue ActiveResource::ClientError => e
-        flash[:error] = YaST::ServiceResource.error(e)
-        ok = false
-    end
-
-    if ok
       begin
-        ok = update.save
-        logger.debug "updated #{patch_id}"
+        update = proxy.find(patch_id)
         rescue ActiveResource::ClientError => e
           flash[:error] = YaST::ServiceResource.error(e)
           ok = false
       end
-      flash[:notice] = _("Patches have been installed.") if ok
-    end
+
+      if ok
+        begin
+          ok = update.save
+          logger.debug "updated #{patch_id}"
+          rescue ActiveResource::ClientError => e
+            flash[:error] = YaST::ServiceResource.error(e)
+            ok = false
+        end
+        flash[:notice] = _("Patch has been installed.") if ok
+      end
     }
     redirect_to({:controller=>"patch_updates", :action=>"index"})
   end
-=begin
-
-  def install
-    proxy = YaST::ServiceResource.proxy_for('org.opensuse.yast.system.patches')
-    ok = true
-    begin
-      update = proxy.find(params[:id])
-      rescue ActiveResource::ClientError => e
-        flash[:error] = YaST::ServiceResource.error(e)
-        ok = false
-    end
-
-    if ok
-      begin
-        ok = update.save
-        rescue ActiveResource::ClientError => e
-          flash[:error] = YaST::ServiceResource.error(e)
-          ok = false
-      end
-      flash[:notice] = _("Patch has been installed.") if ok
-    end
-    redirect_to({:controller=>"patch_updates", :action=>"index"})
-  end
-=end
 end
