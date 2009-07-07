952ee2777ad3e991ef721191d5d9a2de96232ac4
Jiri Suchomel <jsuchome@suse.cz>
2009-07-07 16:27:05 +0200

    adapted for changes in rest-service model

--- plugins/users/app/controllers/users_controller.rb
+++ plugins/users/app/controllers/users_controller.rb
@@ -50,21 +50,18 @@ class UsersController < ApplicationController
   def new
     return unless client_permissions
     @user = @client.new( :id => :nil,
-      :no_home=>nil,
-      :default_group=>nil,
-      :new_login_name=>nil,
-      :login_name=>nil,
-      :groups=>[],
-      :grp_string=>nil,
-      :home_directory=>nil,
-      :full_name=>nil,
-      :uid=>nil,
-      :sshkey=>nil,
-      :new_uid=>nil,
-      :login_shell=>"/bin/bash",
-      :password=>nil,
-      :type=>"local",
-      :id=>nil )
+      :groupname	=> nil,
+      :cn		=> nil,
+      :grouplist	=> {},
+      :homeDirectory	=> nil,
+      :cn		=> nil,
+      :uid		=> nil,
+      :sshkey		=> nil,
+      :loginShell	=> "/bin/bash",
+      :userPassword	=> nil,
+      :type		=> "local",
+      :id		=> nil
+    )
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @user }
@@ -76,7 +73,7 @@ class UsersController < ApplicationController
     return unless client_permissions
     @user = @client.find(params[:id])
     @user.type = ""
-    @user.id = @user.login_name
+    @user.id = @user.uid
     logger.debug "exportssh: #{@user.inspect}"
     respond_to do |format|
       format.html # exportssh.html.erb
@@ -89,11 +86,11 @@ class UsersController < ApplicationController
     return unless client_permissions
 
     @user = @client.find(params[:id])
-    @user.type = ""
-    @user.id = @user.login_name
+
+    @user.type	= ""
+    @user.id	= @user.uid
     @user.grp_string = ""
     counter = 0
-    @user.grp_string = ""
     @user.grouplist.each do |group|
        if counter == 0
           @user.grp_string = group.id
@@ -108,8 +105,8 @@ class UsersController < ApplicationController
   def sshexport
     return unless client_permissions
 
-    @user = @client.find(params["user"]["login_name"])
-    @user.id = @user.login_name
+    @user	= @client.find(params["user"]["uid"])
+    @user.id	= @user.uid
     logger.debug "sshexportssh: #{@user.inspect}"
     @user.sshkey = params["user"]["sshkey"]
     response = true
@@ -145,24 +142,23 @@ class UsersController < ApplicationController
           dummy.groups << { :id=>group.strip }
        end
     end
-    @user = @client.new(:no_home=>params[:nohome],
-                      :default_group=>dummy.default_group,
-                      :new_login_name=>nil,
-                      :login_name=>dummy.login_name,
-                      :groups=>dummy.groups,
-                      :grp_string=>dummy.grp_string,
-                      :home_directory=>dummy.home_directory,
-                      :full_name=>dummy.full_name,
-                      :uid=>dummy.uid,
-                      :sshkey=>nil,
-                      :new_uid=>nil,
-                      :login_shell=>dummy.login_shell,
-                      :password=>dummy.password,
-                      :type=>"local")
+    @user = @client.new(
+	:groupname	=> dummy.groupname,
+        :uid		=> dummy.uid,
+        :grouplist	=> dummy.grouplist,
+#                      :grp_string=>dummy.grp_string,
+        :homeDirectory	=> dummy.homeDirectory,
+	:cn		=> dummy.cn,
+        :uidNumber	=> dummy.uidNumber,
+        :sshkey		=> nil,
+        :loginShell	=> dummy.loginShell,
+        :userPassword	=> dummy.userPassword,
+        :type		=> "local"
+    )
 
     #Only UID greater than 1000 are allowed for local user
     response = true
-    if @user.uid.to_i < 1000
+    if @user.uidNumber.to_i < 1000
       response = false
     else
       begin
@@ -177,7 +173,7 @@ class UsersController < ApplicationController
         flash[:notice] = _('User was successfully created.')
         format.html { redirect_to(users_url) }
       else
-        if @user.uid.to_i < 1000
+        if @user.uidNumber.to_i < 1000
            #Only UID greater than 1000 are allowed for local user
            flash[:error] = _("UID: value >= 1000 is valid for local user only")
         end
@@ -192,27 +188,27 @@ class UsersController < ApplicationController
   def update
     return unless client_permissions
     @user = @client.find(params[:id])
-    @user.new_login_name = nil
-    @user.new_uid = nil
-    @user.id = @user.login_name
-    @user.default_group = params["user"]["default_group"]
-    @user.groups = []
-    if params["user"]["grp_string"] != nil
-      @user.grp_string = params["user"]["grp_string"]
-      params["user"]["grp_string"].split(",").each do |group|
-        @user.groups << { :id=>group.strip }
-      end
-    end
-    if @user.login_name != params["user"]["login_name"]
-      @user.new_login_name = params["user"]["login_name"]
-    end
-    @user.home_directory = params["user"]["home_directory"]
-    @user.full_name = params["user"]["full_name"]
-    if @user.uid != params["user"]["uid"]
-      @user.new_uid = params["user"]["uid"]
-    end
-    @user.login_shell = params["user"]["login_shell"]
-    @user.password = params["user"]["password"]
+    @user.id = @user.uid
+    @user.groupname = params["user"]["groupname"]
+    @user.grouplist = {}
+#    if params["user"]["grp_string"] != nil
+#      @user.grp_string = params["user"]["grp_string"]
+#      params["user"]["grp_string"].split(",").each do |group|
+#        @user.groups << { :id=>group.strip }
+#      end
+#    end
+# FIXME solve renaming...
+#    if @user.login_name != params["user"]["login_name"]
+#      @user.new_login_name = params["user"]["login_name"]
+#    end
+#    if @user.uid != params["user"]["uid"]
+#      @user.new_uid = params["user"]["uid"]
+#    end
+    @user.uidNumber	= params["user"]["uidNumber"]
+    @user.homeDirectory = params["user"]["homeDirectory"]
+    @user.cn = params["user"]["cn"]
+    @user.loginShell = params["user"]["loginShell"]
+    @user.userPassword = params["user"]["userPassword"]
     @user.type = "local"
 
     respond_to do |format|
@@ -239,7 +235,7 @@ class UsersController < ApplicationController
   def destroy
     return unless client_permissions
     @user = @client.find(params[:id])
-    @user.id = @user.login_name
+    @user.id = @user.uid
     @user.type = "local"
     @user.destroy
 
