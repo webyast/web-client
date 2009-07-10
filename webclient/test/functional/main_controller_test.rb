db0f6d21eb1ce4fb07e2bcd6068421086c277793
Klaus Kämpf <kkaempf@suse.de>
2009-07-10 13:46:07 +0200

    Move controlpanel vs. session decision to main controller

--- webclient/test/functional/main_controller_test.rb
+++ webclient/test/functional/main_controller_test.rb
@@ -1,8 +1,22 @@
-require 'test_helper'
+#
+# main_controller_test.rb
+#
+require File.dirname(__FILE__) + '/../test_helper'
 
 class MainControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :accounts
+
+  def test_index_no_session
+    @request.session[:account_id] = nil
+    get :index
+    assert_redirected_to :controller => "session", :action => "new"
+  end
+  
+  def test_index_with_session
+    # Fake an active session
+    # http://railsforum.com/viewtopic.php?id=1719
+    @request.session[:account_id] = Account.find(:first).id
+    get :index
+    assert_redirected_to :controller => "controlpanel", :action => "index"	
   end
 end
