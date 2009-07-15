9e14ef8ac50735ad349724733e4a4eab2acf0b2e
Klaus Kämpf <kkaempf@suse.de>
2009-07-15 15:07:37 +0200

    Start testing Controlpanel controller

--- webclient/test/functional/controlpanel_controller_test.rb
+++ webclient/test/functional/controlpanel_controller_test.rb
@@ -1,8 +1,26 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
+require 'mocha'
+
+require 'active_resource/http_mock'
+
 class ControlpanelControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :accounts, :hosts
+
+  def setup
+    @host = Host.find(1)
+    current_account = Account.new
+    auth_token = "abcdef"
+    Account.stubs(:authenticate).with("quentin","test",@host.url).returns([current_account, auth_token])
+    Account.stubs(:authenticate).with("quentin","bad password",@host.url).returns([nil,nil])
+    Account.stubs(:authenticate).with("quentin","exception",@host.url).raises(RuntimeError)
+    Account.stubs(:authenticate).with("quentin","bad host",@host.url).raises(Errno::ECONNREFUSED)
+    YaST::ServiceResource::Session.site = @host.url
+    ActiveResource::Base.site = @host.url
+  end
+
+  test "controlpanel index" do
+    get :index
+    assert :success
   end
 end
