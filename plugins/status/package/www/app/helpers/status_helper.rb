8250f68051fc6b26bc7b2e7d9f6b52a19aa60317
Klaus Kämpf <kkaempf@suse.de>
2009-07-22 17:30:04 +0200

    Oops, remove bogus files

--- plugins/status/package/www/app/helpers/status_helper.rb
+++
@@ -1,7 +0,0 @@
-module StatusHelper
-  def graph id, width, height, last = false
-    html = "<div id='#{id}' style='width:#{width}px;height:#{height}px;float:left'><img src='/images/spinner.gif'></div>"
-    html += "<br style='clear: both'>" if last
-    return html
-  end
-end
