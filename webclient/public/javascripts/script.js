86b2a2c66f1b0b477efba25b66bd2f44c194a81f
Robert Lihm <rlihm@lihmedia.org>
2009-07-31 12:02:04 +0200

    Changed class name for alternateing rows JS function

--- webclient/public/javascripts/script.js
+++ webclient/public/javascripts/script.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
   });
 
   // Alternating Lines in "Tables"
-  $('.striped-rows > li:even').addClass('alt-bg');
+  // $('.striped-rows > li:even').addClass('alt-bg');
+  $('.alternate-rows > li:even').addClass('alt-bg');
 
 });
