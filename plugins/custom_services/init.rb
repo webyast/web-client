dac459299cf0f7557ed6927cde2e26f3255aa94b
Jiri Suchomel <jsuchome@suse.cz>
2009-08-17 10:02:18 +0200

    custom-services client

---
+++ plugins/custom_services/init.rb
@@ -0,0 +1,12 @@
+# Include hook code here
+# always reload all files in development mode
+if ENV['RAILS_ENV'] == 'development'
+        # get all subdirectories in app/
+	    dirs = Dir[File.join(directory, 'app', '*')].reject{|x| not File.directory?(x)}
+
+	    dirs.each do |dir|
+		        $LOAD_PATH << dir
+			        ActiveSupport::Dependencies.load_paths << dir
+				        ActiveSupport::Dependencies.load_once_paths.delete(dir)
+	    end
+end
