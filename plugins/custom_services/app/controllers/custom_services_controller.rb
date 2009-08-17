89703cf8f4b2848c1bfbdb71de8df8414d0f5a96
Jiri Suchomel <jsuchome@suse.cz>
2009-08-17 10:02:18 +0200

    custom-services client

---
+++ plugins/custom_services/app/controllers/custom_services_controller.rb
@@ -0,0 +1,67 @@
+require 'yast/service_resource'
+
+class CustomServicesController < ApplicationController
+  before_filter :login_required
+  layout 'main'
+
+  private
+  def client_permissions
+    @client = YaST::ServiceResource.proxy_for('org.opensuse.yast.modules.yapi.services')
+    unless @client
+      flash[:notice] = _("Invalid session, please login again.")
+      redirect_to( logout_path ) and return
+    end
+    @permissions = @client.permissions
+  end
+
+  # Initialize GetText and Content-Type.
+  init_gettext "yast_webclient_custom_service"  # textdomain, options(:charset, :content_type)
+
+  public
+
+  def initialize
+  end
+
+  # GET /services
+  # GET /services.xml
+  def index
+    return unless client_permissions
+    @services = []
+logger.debug "-------------------------------- index"
+
+    begin
+      @services = @client.find(:all)
+      rescue ActiveResource::ClientError => e
+        flash[:error] = YaST::ServiceResource.error(e)
+    end
+logger.debug @services.inspect
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @services }
+    end
+  end
+
+  def execute
+    return unless client_permissions
+    @service = @client.find(params[:service_id])
+
+    response = @client.put(params[:service_id], :execute => params[:id])
+
+    # we get a hash with exit, stderr, stdout
+    ret = Hash.from_xml(response.body)
+    ret = ret["hash"]
+    logger.debug "returns #{ret.inspect}"
+    
+    @result_string = ""
+    @result_string << ret["stdout"] if ret["stdout"]
+    @result_string << ret["stderr"] if ret["stderr"]
+    @error_string = ret["exit"].to_s
+    if ret["exit"] == 0
+       @error_string = _("success")
+    end
+    render(:partial =>'result')
+  end
+
+
+end
