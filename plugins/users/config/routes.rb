c9fcc2d3ea06d00b5fbfa13c71436502867e4322
Josef Reidinger <jreidinger@suse.cz>
2009-08-17 15:16:22 +0200

    Don't use private routing in users frontend

--- plugins/users/config/routes.rb
+++
@@ -1,5 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  #map.resources :users
-  map.resources :users, :member => { :exportssh => :get }
-  map.connect "/users/:users_id/exportssh", :controller => 'users', :action => 'sshexport'
-end
