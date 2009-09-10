7ead49b79d3462633c79bbae19babdd9f20d3b4f
Josef Reidinger <jreidinger@suse.cz>
2009-09-10 17:01:15 +0200

    use localized error messages for backend problems

--- webclient/lib/error_constructor.rb
+++ webclient/lib/error_constructor.rb
@@ -3,7 +3,17 @@
 
 module ErrorConstructor    
   def construct_error (error)
-    #now use stupid unlocalized message
-    return error["error"]["description"]
+
+    error = error["error"]
+    case error["type"]
+    when "NO_PERM"
+      return _("Permission #{error["permission"]} is not granted for user #{error["user"]}")
+    when "POLKIT"
+      return _("Policy kit exception for user #{error["user"]} and permission #{error["permission"]} (untranslated message): #{error["message"]}")
+    when "NOT_LOGGED"
+      return _("Noone is logged to rest service.")
+    when "BADFILE"
+      return _("Target system is not consistent: Missing or corrupted file #{@file}")
+    end
   end
 end
