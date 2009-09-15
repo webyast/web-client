f538c7051ed91e0917cdff52a4dc860c3f2180eb
Martin Vidner <mvidner@suse.cz>
2009-09-15 14:24:57 +0200

    Do not use string_serialization, it breaks Hash#to_xml :-(

--- plugins/network/lib/string_serialization.rb
+++ plugins/network/lib/string_serialization.rb
@@ -1,5 +1,18 @@
 # fixing serialization of arrays of plain strings
 
+# DO NOT USE, it breaks Hash#to_xml:
+# h = {"foo" => "bar", "baz" => "qux"}
+# before:
+# <hash>
+#   <baz>qux</baz>
+#   <foo>bar</foo>
+# </hash>
+# after:
+# <hash>
+#   <string>qux</string>
+#   <string>bar</string>
+# </hash>
+
 class String
   def to_xml(options = {})
     require 'builder' unless defined?(Builder)
