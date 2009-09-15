92c2845d07fbd31c9b8330f6f2a4fa716565a3ee
Josef Reidinger <jreidinger@suse.cz>
2009-09-15 09:06:26 +0200

    Cancel redirect to control panel instead previous page ( problem with page that has some subpages), wizard is on main page and cancel there is final cancel

--- webclient/app/helpers/application_helper.rb
+++ webclient/app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def form_send_buttons (send_options={})
         if session[:wizard_current].nil? or session[:wizard_current] == "FINISH"
           submit = submit_tag _("Save"),send_options
-          back = link_to _("Cancel"), :back
+          back = link_to _("Cancel"), :controller => "controlpanel"
           return submit+back
         else
           back = link_to "Back", :controller => "controlpanel", :action => "backstep"
