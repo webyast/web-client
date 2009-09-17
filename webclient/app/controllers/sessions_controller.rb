7b80250fffaa6346793fdaac498d581ca1a84774
Stefan Schubert <schubi@suse.de>
2009-09-17 10:29:04 +0200

    render the correct view for index action; Bug 519740

--- webclient/app/controllers/sessions_controller.rb
+++ webclient/app/controllers/sessions_controller.rb
@@ -11,7 +11,9 @@ class SessionsController < ApplicationController
   #before_filter :ensure_logout, :only => [:new, :create]
   
   def index
-    # only used to display the flash message
+    #This is used in order to support the browser "back" button
+    new
+    return
   end
 
   # shows the current session
@@ -23,12 +25,6 @@ class SessionsController < ApplicationController
   #  render login screen
   #
   def new
-    # we can't create session if we are logged in
-    if logged_in?
-      redirect_to :action => "index"
-      return
-    end
-
     # Set @host to display info at login screen
     @host = Host.find(params[:hostid]) rescue nil
 
