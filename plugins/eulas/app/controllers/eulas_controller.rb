f313c0edf297819a0cc24b2fd02d0e458fd44d80
Martin Kudlvasr <mkudlvasr@suse.cz>
2009-10-14 21:43:20 +0200

    finally working eula client

--- plugins/eulas/app/controllers/eulas_controller.rb
+++ plugins/eulas/app/controllers/eulas_controller.rb
@@ -1,9 +1,11 @@
 require "yast/service_resource"
+require 'enumerator'
 
 
 class EulasController < ApplicationController
   before_filter :login_required
   before_filter :ensure_eula_list, :only => [:next, :show]
+  before_filter :ensure_id, :only => [:show, :update, :update_text, :show_text]
 
   layout 'main'
   include ProxyLoader
@@ -11,11 +13,24 @@ class EulasController < ApplicationController
   # Initialize GetText and Content-Type.
   init_gettext "yast_webclient_language"  # textdomain, options(:charset, :content_type)
 
+  def load_proxy_without_permissions (*args)
+    proxy = YaST::ServiceResource.proxy_for args[0]
+    raise "No proxy_for for #{args[0]}" if proxy.nil?
+    found_objects = proxy.find *(args[1..-1])
+    raise "Error when proxy.find for #{args[0]}" if found_objects.nil?
+    found_objects # can be array or single
+  end
+
   def ensure_eula_list
-    if session[:eula_unaccepted_ids].nil? then
-      eulas = load_proxy "org.opensuse.yast.modules.eulas", :all
-      session[:eula_unaccepted_ids] = eulas.each_with_index{|eula,index| eula.accepted ? nil : (index+1)}.compact
+    if session[:eula_unaccepted_ids_cache].nil? then
+      eulas = load_proxy_without_permissions "org.opensuse.yast.modules.eulas", :all
+      session[:eula_unaccepted_ids_cache] = eulas.to_enum(:each_with_index).collect{|eula,index| eula.accepted ? nil : (index+1)}.compact
     end
+  end
+
+  def ensure_id
+    redirect_to :action => :next and return if params[:id].nil?
+    @eula_id   = params[:id]
   end
 
   def index 
@@ -23,18 +38,47 @@ class EulasController < ApplicationController
   end
 
   def next
-    next_id = session[:eula_unaccepted_ids][0]
+    next_id = session[:eula_unaccepted_ids_cache][0]
     if next_id.nil? then
-      render :all_accepted
+      redirect_success
     else
-      redirect_to :action => :show, :id => next_id
+      redirect_to :action => :show, :id => next_id, :eula_lang => params[:eula_lang]
     end
   end
 
-  def show
-    redirect_to :action => :next and return if params[:id].nil?
-    @eula      = load_proxy "org.opensuse.yast.modules.eulas", params[:id].nil?
-    @last_eula = session[:eula_unaccepted_ids].length == 1
+  def update_text
+    eula_params = params[:lang].nil? ? Hash.new : { :lang => params[:lang][0] }
+    @eula = load_proxy_without_permissions "org.opensuse.yast.modules.eulas", @eula_id, :params => eula_params
+    render :update do |page|
+      page.replace_html 'eula-text', @eula.text
+    end
   end
+
+  def show_text
+    eula_params = params[:lang].nil? ? Hash.new : { :lang => params[:lang][0] }
+    @eula = load_proxy_without_permissions "org.opensuse.yast.modules.eulas", @eula_id, :params => eula_params
+    render :text => @eula.text
+  end
+
+  def show
+    eula_params = params[:eula_lang].nil? ? Hash.new : { :lang => params[:eula_lang] }
+    @eula = load_proxy_without_permissions "org.opensuse.yast.modules.eulas", @eula_id, :params => eula_params
+  end
+
+  def update
+    @eula                 = load_proxy_without_permissions "org.opensuse.yast.modules.eulas", @eula_id
+    @eula.text            = ""
+    @eula.available_langs = []
+    @eula.accepted        = params[:accepted]
+    @eula.id              = @eula_id # XXX Why is this not set by the load_proxy !!!!
+    @eula.save
+    if @eula.accepted
+      session[:eula_unaccepted_ids_cache].shift
+    else
+      flash[:error] = _("You must accept all licences before using this product!")
+    end
+    redirect_to :action => :next, :eula_lang => params[:eula_lang]
+  end
+
 
 end
