b14480778cae501d90c572eac0c1b93eb737669f
Robert Lihm <rlihm@lihmedia.org>
2009-10-21 12:31:32 +0200

    * added string to "form_send_buttons" * added space above footer

--- webclient/app/helpers/application_helper.rb
+++ webclient/app/helpers/application_helper.rb
@@ -1,14 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def form_send_buttons (send_options={})
+        str_spacer = _(' or ')
         if session[:wizard_current].nil? or session[:wizard_current] == "FINISH"
           submit = submit_tag _("Save"),send_options
           back = link_to _("Cancel"), :controller => "controlpanel"
-          return submit+back
+          return submit+str_spacer+back
         else
           back = link_to "Back", :controller => "controlpanel", :action => "backstep"
           nextb = submit_tag _("Next"),send_options
-          return back+nextb
+          return back+str_spacer+nextb
         end
   end
 end
