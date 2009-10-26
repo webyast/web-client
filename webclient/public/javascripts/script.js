e8bf30a1a9ace43b6902fd2588cd9d5a6e5210e8
Robert Lihm <rlihm@lihmedia.org>
2009-10-26 16:40:14 +0100

    made base for tabed menu Missing: tab-look & feel

--- webclient/public/javascripts/script.js
+++ webclient/public/javascripts/script.js
@@ -13,18 +13,42 @@ $(document).ready(function() {
 
 // Tab-UI ====================================================================
 
-$('.ui-tab-parent').tabs();
+// $('.ui-tab-parent').tabs('.ui-tab-parent > fieldset');
+// $('.tabs').tabs('fieldset');
 
-$('.ui-tab-parent').bind('tabsselect', function(event, ui) {
+// $('.ui-tab-parent').bind('tabsselect', function(event, ui) {
+// 
+//     // Objects available in the function context:
+//     // ui.tab     // anchor element of the selected (clicked) tab
+//     // ui.panel   // element, that contains the selected/clicked tab contents
+//     // ui.index   // zero-based index of the selected (clicked) tab
+//     
+//     
+// 
+// });
 
-    // Objects available in the function context:
-    // ui.tab     // anchor element of the selected (clicked) tab
-    // ui.panel   // element, that contains the selected/clicked tab contents
-    // ui.index   // zero-based index of the selected (clicked) tab
-    
-    
+$('.tab-form fieldset').hide();
 
+var arrTabs = new Array;
+
+$('.tab-form fieldset legend').each(function(index) {
+  var tabTitle = $(this).html();  // get string from legent to have a tab-title
+  var tabID = tabTitle.toLowerCase().replace(' ', '_'); // create id compatibel string
+  
+  $('.tab-nav').append('<li><a href="#' + tabID + '">' + tabTitle + '</a></li>'); // make tab bar
+  
+  $(this).parent('fieldset').attr('id', tabID);
+  
+  $('.tab-form fieldset:first').show();
 });
+
+$('.tab-nav a').click(function() {
+  // var curTab = $(this).attr('href').substr(1);
+  var curTab = $(this).attr('href');
+  // alert(curTab);
+  $(curTab).show().siblings('fieldset').hide();
+});
+
 
 
 
