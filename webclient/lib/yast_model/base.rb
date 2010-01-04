92aaa80d82a80608d2190b3d295a19f226b60961
Josef Reidinger <jreidinger@suse.cz>
2010-01-04 15:58:59 +0100

    fix passing parameter to connection call, move testing valid connection attributes to separate function

--- webclient/lib/yast_model/base.rb
+++ webclient/lib/yast_model/base.rb
@@ -1,65 +1,74 @@
 module YastModel
   module Base
 
-      def connection
-        #check if connection is still valid
-        if self.site.nil? || self.site != URI.parse(YaST::ServiceResource::Session.site) || self.password != YaST::ServiceResource::Session.auth_token
-          set_site
-        end
-        super
+    def valid_connection?(site = self.site, password = self.password)
+     return !(
+         site.blank? ||
+         site != URI.parse(YaST::ServiceResource::Session.site) ||
+         password != YaST::ServiceResource::Session.auth_token
+         )
+    end
+
+    def connection(*args)
+      #check if connection is still valid
+      unless valid_connection?
+        set_site
       end
+      super args
+    end
 
-      def model_interface(i)
-        @interface = i.to_sym
+    def model_interface(i)
+      @interface = i.to_sym
+    end
+
+    def interface
+      @interface
+    end
+
+    def site
+      ret = super
+      Rails.logger.debug "read interface to #{@interface.to_s}"
+      Rails.logger.debug "site is #{ret.to_s} and should be #{YaST::ServiceResource::Session.site}"
+      Rails.logger.debug "pwd is #{password} and should be #{YaST::ServiceResource::Session.auth_token}"
+
+      if valid_connection? ret
+        return ret
+      else
+        set_site
+        return super #call again super to properly call all hooks after set new site
       end
+    end
 
-      def interface
-        @interface
+    def set_site
+      @permissions = nil #reset permission as it can be for each site and for each user different
+      self.site = YaST::ServiceResource::Session.site
+      self.password = YaST::ServiceResource::Session.auth_token
+      YastModel::Resource.site = "#{self.site}/" #resource has constant prefix to allow introspect
+      #FIXME not thread safe
+      Rails.logger.debug "read interface to #{@interface.to_s}"
+      Rails.logger.debug "set site tot #{self.site}"
+      Rails.logger.debug "set token to #{self.password}"
+      resource = YastModel::Resource.find(:all).find { |r| r.interface.to_sym == @interface.to_sym }
+      #TODO throw exception if not found
+      p, sep, self.collection_name = resource.href.rpartition('/')
+      p += '/'
+      self.prefix = p
+      self.element_name = collection_name
+    end
+
+    #fix ARs broken singleton
+    def find_one(options)
+      case from = options[:from]
+      when Symbol
+        instantiate_record(get(from, options[:params]))
+      when String
+        path = "#{from}#{query_string(options[:params])}"
+        instantiate_record(connection.get(path, headers))
+      else
+        prefix_options, query_options = split_options(options[:params])
+        path = self.collection_path(prefix_options, query_options)
+        instantiate_record( (connection.get(path, headers)), prefix_options )
       end
-
-      def site
-        ret = super
-        Rails.logger.debug "read interface to #{@interface.to_s}"
-        Rails.logger.debug "site is #{ret.to_s} and should be #{YaST::ServiceResource::Session.site}"
-        Rails.logger.debug "pwd is #{password} and should be #{YaST::ServiceResource::Session.auth_token}"
-
-        if ret.nil? || ret != URI.parse(YaST::ServiceResource::Session.site) || password != YaST::ServiceResource::Session.auth_token
-          Rails.logger.debug "set new site for interface #{@interface}"
-          set_site
-        end
-        return super
-      end
-
-      def set_site
-        @permissions = nil #reset permission as it can be for each site and for each user different
-        self.site = YaST::ServiceResource::Session.site
-        self.password = YaST::ServiceResource::Session.auth_token
-        YastModel::Resource.site = "#{self.site}/" #resource has constant prefix to allow introspect
-        #FIXME not thread save
-        Rails.logger.debug "read interface to #{@interface.to_s}"
-        Rails.logger.debug "set site tot #{self.site}"
-        Rails.logger.debug "set token to #{self.password}"
-        resource = YastModel::Resource.find(:all).find { |r| r.interface.to_sym == @interface.to_sym }
-        #TODO throw exception if not find
-        p, sep, self.collection_name = resource.href.rpartition('/')
-        p += '/'
-        self.prefix = p
-        self.element_name = collection_name
-      end
-
-      #fix ARs broken singleton
-      def find_one(options)
-        case from = options[:from]
-        when Symbol
-          instantiate_record(get(from, options[:params]))
-        when String
-          path = "#{from}#{query_string(options[:params])}"
-          instantiate_record(connection.get(path, headers))
-        else
-          prefix_options, query_options = split_options(options[:params])
-          path = self.collection_path(prefix_options, query_options)
-          instantiate_record( (connection.get(path, headers)), prefix_options )
-        end
     end
 
     def permissions
