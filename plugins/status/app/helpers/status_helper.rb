2a684b74524e21d94b9878980da0f4d6000c40be
Stefan Schubert <schubi@suse.de>
2010-01-11 18:21:22 +0100

    added waiting clock

--- plugins/status/app/helpers/status_helper.rb
+++ plugins/status/app/helpers/status_helper.rb
@@ -23,7 +23,7 @@ module StatusHelper
   end
 
   def evaluate_next_graph group, single_graphs, index
-    return nil if index+1 > single_graphs.size
+    return "$('#progress').hide()" if index+1 > single_graphs.size
     graph_div_id = graph_id(group, single_graphs[index].headline)
     remote_function(:update => graph_div_id,
                     :url => { :action => "evaluate_values", :group_id => group, :graph_id => single_graphs[index].headline},
