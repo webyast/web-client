4dc04fdeefa18848a8737b784d09e58d3795a6c2
Stefan Schubert <schubi@suse.de>
2010-01-20 16:23:17 +0100

    first running testcases

--- plugins/status/test/functional/status_controller_test.rb
+++ plugins/status/test/functional/status_controller_test.rb
@@ -7,6 +7,10 @@ require 'yast_mock'
 
 class StatusControllerTest < ActionController::TestCase
 
+  # return contents of a fixture file +file+
+  def fixture(file)
+    IO.read(File.join(File.dirname(__FILE__), "..", "fixtures", file))
+  end
 
   def setup
     StatusController.any_instance.stubs(:login_required)
@@ -14,17 +18,18 @@ class StatusControllerTest < ActionController::TestCase
     @request = ActionController::TestRequest.new
     # http://railsforum.com/viewtopic.php?id=1719
     @request.session[:account_id] = 1 # defined in fixtures
-    response_logs = IO.read(File.join(File.dirname(__FILE__),"..","fixtures","logs.xml"))
-    response_graphs = IO.read(File.join(File.dirname(__FILE__),"..","fixtures","graphs.xml"))
-    response_metrics = IO.read(File.join(File.dirname(__FILE__),"..","fixtures","metrics.xml"))
+    response_logs = fixture "logs.xml"
+    response_graphs = fixture "graphs.xml"
+    response_metrics = fixture "metrics.xml"
     ActiveResource::HttpMock.set_authentication
     ActiveResource::HttpMock.respond_to do |mock|
-      header = ActiveResource::HttpMock.authentication_header
+      @header = ActiveResource::HttpMock.authentication_header
       mock.resources  :"org.opensuse.yast.system.logs" => "/logs", :"org.opensuse.yast.system.metrics" => "/metrics", :"org.opensuse.yast.system.graphs" => "/graphs"
       mock.permissions "org.opensuse.yast.system.status", { :read => true, :writelimits => true }
-      mock.get   "/logs.xml", header, response_logs, 200
-      mock.get   "/graphs.xml", header, response_graphs, 200
-      mock.get   "/metrics.xml", header, response_metrics, 200
+      mock.get   "/logs.xml", @header, response_logs, 200
+      mock.get   "/graphs.xml?checklimits=true", @header, response_graphs, 200
+      mock.get   "/graphs.xml", @header, response_graphs, 200
+      mock.get   "/metrics.xml", @header, response_metrics, 200
     end
   end
 
@@ -32,42 +37,55 @@ class StatusControllerTest < ActionController::TestCase
     ActiveResource::HttpMock.reset!
   end
 
+  #first index call
   def test_index
     get :index
-#    assert_response :success
-#    assert_valid_markup
-#    assert assigns(:graphs)
+    assert_response :success
+    assert_valid_markup
+    assert assigns(:graphs)
   end
 
+  # status module must survive collectd out of sync
+  def test_collectd_out_of_sync
+    response_graphs = fixture "out_sync_error.xml"
+    response_metrics = fixture "out_sync_error.xml"
+    response_logs = fixture "logs.xml"
 
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.resources  :"org.opensuse.yast.system.logs" => "/logs", :"org.opensuse.yast.system.metrics" => "/metrics", :"org.opensuse.yast.system.graphs" => "/graphs"
+      mock.permissions "org.opensuse.yast.system.status", { :read => true, :writelimits => true }
+      mock.get   "/logs.xml", @header, response_logs, 200
+      mock.get   "/graphs.xml?checklimits=true", @header, response_graphs, 503
+      mock.get   "/graphs.xml", @header, response_graphs, 503
+      mock.get   "/metrics.xml", @header, response_metrics, 503
+    end
 
-#OUT_SYNC_ERROR = <<EOF
-#<error>
-#  <type>COLLECTD_SYNC_ERROR</type>
-#  <description>blba bla</description>
-#</error>
-#EOF
-#
-#class ResponseMock
-#  def body
-#    return OUT_SYNC_ERROR
-#  end
-#
-#  def code
-#    return "503"
-#  end
-#end
-#
-#status module must survive collectd out of sync
-#  def test_collectd_out_of_sync
-#    YaST::ServiceResource.stubs(:proxy_for).with('org.opensuse.yast.system.logs').returns(@logs_proxy)
-#    sproxy = StatusProxy.new
-#    YaST::ServiceResource.stubs(:proxy_for).with('org.opensuse.yast.system.status').returns(sproxy)
-#    sproxy.stubs(:find).raises(ActiveResource::ServerError.new(ResponseMock.new,""))
-#    get :index
-#    assert_response :success       will be obsolete
-#    assert_valid_markup
-#    assert assigns(:logs)
-#  end
+    ret = get :index
+    assert_response :success
+    assert_valid_markup
+    assert_tag "Collectd is out of sync. Status information can be expected at Wed Jan 20 22:34:38 2010."
+
+  end
+
+  # status module must survive SERVICE_NOT_RUNNING
+  def test_collectd_service_not_running
+    response_graphs = fixture "service_not_running.xml"
+    response_metrics = fixture "service_not_running.xml"
+    response_logs = fixture "logs.xml"
+
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.resources  :"org.opensuse.yast.system.logs" => "/logs", :"org.opensuse.yast.system.metrics" => "/metrics", :"org.opensuse.yast.system.graphs" => "/graphs"
+      mock.permissions "org.opensuse.yast.system.status", { :read => true, :writelimits => true }
+      mock.get   "/logs.xml", @header, response_logs, 200
+      mock.get   "/graphs.xml?checklimits=true", @header, response_graphs, 503
+      mock.get   "/graphs.xml", @header, response_graphs, 503
+      mock.get   "/metrics.xml", @header, response_metrics, 503
+    end
+
+    ret = get :index
+    assert_response :success
+    assert_valid_markup
+    assert_tag "collectd is not running on the target machine"
+  end
 
 end
