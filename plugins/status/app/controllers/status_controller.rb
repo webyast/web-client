3fcec5afac8ed92f9258f6ad88e7ed1193e32d38
Stefan Schubert <schubi@suse.de>
2010-01-20 17:35:29 +0100

    show summary AJAX call

--- plugins/status/app/controllers/status_controller.rb
+++ plugins/status/app/controllers/status_controller.rb
@@ -11,7 +11,6 @@ class StatusController < ApplicationController
   private
   def client_permissions
     @permissions = Status.permissions
-puts "xxxxxxxxxxxxxxxxaaa #{@permissions.inspect}"
   end
 
   #
--- plugins/status/test/functional/status_controller_test.rb
+++ plugins/status/test/functional/status_controller_test.rb
@@ -62,6 +62,36 @@ class StatusControllerTest < ActionController::TestCase
     assert assigns(:graphs)
   end
 
+  #testing show summary AJAX call; OK
+  def test_show_summary
+    ret = get :show_summary
+    assert_response :success
+    assert_valid_markup
+    assert_tag :tag =>"div",
+               :attributes => { :class => "status-icon ok" }
+    assert_tag "Your system is healthy."
+  end
+
+  #testing show summary AJAX call; limit CPU user reached
+  def test_show_summary_limit_reached
+    response_graphs = fixture "graphs_limits_reached.xml"
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.resources  :"org.opensuse.yast.system.logs" => "/logs", :"org.opensuse.yast.system.metrics" => "/metrics", :"org.opensuse.yast.system.graphs" => "/graphs"
+      mock.permissions "org.opensuse.yast.system.status", { :read => true, :writelimits => true }
+      mock.get   "/logs.xml", @header, @response_logs, 200
+      mock.get   "/graphs.xml?checklimits=true", @header, response_graphs, 200
+      mock.get   "/graphs.xml", @header, response_graphs, 200
+      mock.get   "/metrics.xml", @header, @response_metrics, 200
+    end
+
+    ret = get :show_summary
+    assert_response :success
+    assert_valid_markup
+    assert_tag :tag =>"div",
+               :attributes => { :class => "status-icon error" }
+    assert_tag "Limits exceeded for CPU/CPU-0/user"
+  end
+
   # status module must survive collectd out of sync
   def test_collectd_out_of_sync
     response_graphs = fixture "out_sync_error.xml"
