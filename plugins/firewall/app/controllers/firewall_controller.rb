9cd0a179ef18fbf1625c002a84e24d4b09140ff5
Martin Kudlvasr <mkudlvasr@novell.com>
2010-01-29 22:05:13 +0100

    small changes due to changes in rest-service. firewall now shows live data.

--- plugins/firewall/app/controllers/firewall_controller.rb
+++ plugins/firewall/app/controllers/firewall_controller.rb
@@ -8,7 +8,7 @@ class FirewallController < ApplicationController
   init_gettext "yast_webclient_firewall"
 
   def index
-    flash[:error] = "These data are only a sample, they do not correspond to machine settings!"
+    flash[:error] = "Saving the configuration doesn't work yet!"
     flash[:notice] = "IDEA 1: Do not use 'Use firewall' checkbox. Firewall will be used implicitely unless all services are allowed."
     flash[:warning] = "IDEA 2: Can be combined with services module by adding 'allowed' checkbox to each service. Pros : simpler interface, Cons : no visible firewall icon in control panel (it will be harder to market firewall to customer). "
     @firewall    = Firewall.find :one
