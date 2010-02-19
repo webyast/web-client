67e546d73da7fcaf76741a764eeeec76f510f68b
J. Daniel Schmidt <jdsn@suse.de>
2010-02-19 17:43:06 +0100

    intermediate fixes for registration issues

--- plugins/registration/app/controllers/registration_controller.rb
+++ plugins/registration/app/controllers/registration_controller.rb
@@ -9,7 +9,7 @@ class RegistrationController < ApplicationController
   include ProxyLoader
 
   def initialize
-    @trans = { 'email' => _("Email"),
+    @trans = {  'email' => _("Email"),
                 'moniker' => _("System name"),
                 'regcode-sles' => _("SLES registration code"),
                 'regcode-sled' => _("SLED registration code"),
@@ -52,86 +52,88 @@ class RegistrationController < ApplicationController
   # Initialize GetText and Content-Type.
   init_gettext "yast_webclient_registration"  # textdomain, options(:charset, :content_type)
 
+  def client_guid
+    begin
+      status = @client.find
+      @guid = status.guid unless status.guid.blank?
+      logger.debug "found GUID: #{@guid}"
+    rescue
+      @guid = nil
+      logger.debug "no GUID found"
+    end
+
+    return @guid
+  end
 
   public
 
   # Index handler. Loads information from backend and if success all required
-  # fields is filled. In case of errors redirect to help page, main page or just
+  # fields are filled. In case of errors redirect to help page, main page or just
   # show flash with partial problem.
   def index
     return unless client_permissions
 
-    @detail = action_name == "detail"
-
     unless @permissions[:statelessregister]
       flash[:warning] = _("No permissions for registration")
       redirect_to root_path
       return false
     end
 
-    status = nil
-    @guid = nil
-    begin
-      status = @client.find if @client
-    rescue
-      status = nil
+    if !client_guid
+      @arguments = []
+      update
     end
 
-    if status && status.respond_to?('guid') then
-      @guid = status.guid unless status.guid.blank?
-      # flash[:error] = "YES we have a GUI: #{status.guid}"
-    else
-      begin
-        register = @client.new( {:arguments=>nil,
-                                :options=>@options} )
-        register.save
-      rescue ActiveResource::ClientError => e
-        error = Hash.from_xml(e.response.body)["registration"]
-        if error && error["status"] == "missinginfo" && !error["missingarguments"].blank?
-          logger.debug "missing arguments #{error["missingarguments"].inspect}"
-          @arguments = sort_arguments( error["missingarguments"] ) #in order to show it in an unique order
-        else
-          logger.error "error while getting arguments: #{error.inspect}"
-          # keep the old message for the translation, in case we need it later
-          old_error_msg = _("Arguments for registration cannot be evaluated.")
-          flash[:error] = _("An error occurred during registration. Please try again.")
-          redirect_to root_path
-          return false
-        end
-      end
-    end
   end
 
   def detail
     # different action but same semantic as in update
+    @detail = true
     update
   end
 
-  # Calling the register over the service
+  def reregister
+    # provide a way to force a new registration, even if system is already registered
+    @reregister = true
+    update
+  end
+
+  # Calling registration over the service
   def update
     return unless client_permissions
-
-    @detail = action_name == "detail"
 
     unless @permissions[:statelessregister]
       flash[:warning] = _("No permissions for registration")
       redirect_to root_path
       return false
     end
-    params.each {| key, value |
-      if key.starts_with? "registration_arg,"
-        argument = Hash.new
-        argument["name"] = key[17, key.size-17]
-        argument["value"] = value
-        @arguments << argument
+
+    if client_guid && @reregister
+      flash[:notice] = _("This system is already registered.")
+      redirect_success
+      return
+    end
+
+
+    begin
+      params.each do | key, value |
+        if key.starts_with? "registration_arg,"
+          argument = Hash.new
+          argument["name"] = key[17, key.size-17]
+          argument["value"] = value
+          @arguments << argument
+        end
       end
-    }
+    rescue
+      # @arguments = nil
+      logger.debug "No arguments were passed to the registration call."
+    end
 
     @changed_repositories = []
     @changed_services = []
     success = false
     begin
-      register = @client.create({:arguments=> { 'argument' => @arguments }, 
+      register = @client.create({:arguments=> { 'argument' => @arguments },
                                 :options=>@options})
       logger.debug "registration finished: #{register.to_xml}"
       @changed_repositories = register.changedrepos if register.respond_to? :changedrepos
@@ -140,7 +142,7 @@ class RegistrationController < ApplicationController
       success = true
 
       # inform about added services and its catalogs
-      if @changed_services && @changed_services.kind_of?(Array) then
+      if @changed_services && @changed_services.kind_of?(Array) && @changed_services.size > 0 then
         flash[:notice] += "<ul>"
         @changed_services.each do |c|
           if c.respond_to?(:name) && c.name && c.respond_to?(:status) && c.status == 'added' then
@@ -162,6 +164,7 @@ class RegistrationController < ApplicationController
         end
         flash[:notice] += "</ul>"
       else
+##FIXME - also care about repos!!!
         # display warning message if no repos are added/changed during registration (bnc#558854)
         flash[:warning] = _("<p><b>Repositories were not modified during the registration process.</b></p><p>It is likely that an incorrect registration code was used. If this is the case, please attempt the registration process again to get an update repository.</p><p>Please make sure that this system has an update repository configured, otherwise it will not receive updates.</p>")
         # flash[:warning] = _("No repositories were added or changed during the registration process (maybe due to a wrong registration code). If this system already has an update repository everything is fine. But without an update repository this system will not receive any updates. You may run the registration module again.")
@@ -205,7 +208,12 @@ class RegistrationController < ApplicationController
         flash[:error] = _("Please fill out missing entries.")
       else
         logger.error "error while registration: #{error.inspect}"
-        flash[:error] = _("Error occured while connecting to registration server.")
+        error_old = _("Error occured while connecting to registration server.")
+        error_line1 = _("An error occured while connecting to the registration server.")
+        error_line2 = _("Please try to register again later.")
+        flash[:error] = error_line1 + "<br>" + error_line2
+        # success: allow users to skip registration in case of an error (bnc#578684) (bnc#579463)
+        success = true
       end
     end
     @arguments = sort_arguments( @arguments ) #in order to show it in an unique order
