2a1f493bff2e2169ed4517dc42b56bcaaa975859
Jiri Suchomel <jsuchome@suse.cz>
2010-03-17 08:52:04 +0100

    shortcuts for accesing object data

--- plugins/services/app/models/service.rb
+++ plugins/services/app/models/service.rb
@@ -1,4 +1,12 @@
 class Service < YaST::ServiceResource::Base
   extend YastModel::Base
   model_interface :"org.opensuse.yast.modules.yapi.services"
+
+  def enabled?
+    return self.enabled == "true"
+  end
+
+  def custom?
+    return self.custom == "true"
+  end
 end
