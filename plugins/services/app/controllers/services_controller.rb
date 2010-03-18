c0fd862c38fba6c3396b119e9a3975845106ae99
Jiri Suchomel <jsuchome@suse.cz>
2010-03-18 07:52:55 +0100

    no enabled/disabled check for custom services buttons moved below the status text

--- plugins/services/app/controllers/services_controller.rb
+++ plugins/services/app/controllers/services_controller.rb
@@ -23,7 +23,11 @@ class ServicesController < ApplicationController
 	render :text => _('(cannot read status)') and return
     end
 
-    render(:partial =>'status', :locals => { :status => @response.status, :enabled => @response.enabled }, :params => params)
+    render(
+	:partial =>'status',
+	:locals	=> { :status => @response.status, :enabled => @response.enabled?, :custom => @response.custom? },
+	:params => params
+    )
   end
 
   # GET /services
