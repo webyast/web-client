3dbd2eafef28f81f2bf0e052e797999878b2a0a8
Ladislav Slezak <lslezak@novell.com>
2010-03-19 15:26:55 +0100

    status - better autorefresh logging

--- plugins/status/app/controllers/status_controller.rb
+++ plugins/status/app/controllers/status_controller.rb
@@ -380,13 +380,18 @@ class StatusController < ApplicationController
 
   private
 
-  def refresh_timout
+  def refresh_timeout
     timeout = ControlPanelConfig.read 'system_status_timeout'
     if timeout.nil?
       timeout = 60
-      Rails.logger.warn "Cannot read 'system_status_timeout' value using default: #{timeout}"
+      Rails.logger.warn "Cannot read 'system_status_timeout', value using default: #{timeout}"
     end
-    Rails.logger.info "Autorefresh system status after #{timeout} seconds"
+
+    if timeout.zero?
+      Rails.logger.info "System status autorefresh is disabled"
+    else
+      Rails.logger.info "Autorefresh system status after #{timeout} seconds"
+    end
 
     return timeout
   end
