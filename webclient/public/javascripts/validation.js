01d0f5df0ee2b5a5624141833800d61363805165
Vladislav Gorobets <vgorobets@suse.de>
2010-04-12 11:45:09 +0200

    - fixed missing translation for smtp error message and smtp server field should accept port number

--- webclient/public/javascripts/validation.js
+++ webclient/public/javascripts/validation.js
@@ -8,8 +8,12 @@
 var protocol = "(http|https|ftp)";
 // FQDN: http://regexlib.com/REDetails.aspx?regexp_id=391
 var host = "([a-zA-Z0-9](([\\.\\-]?[a-zA-Z0-9]+){0,61}[a-zA-Z0-9]))+[a-zA-Z0-9]*";
+
+
 // FQDN: http://regexlib.com/REDetails.aspx?regexp_id=391
 var fqdn = "([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}";
+var port ="(:[0-9]{1,5}|)";
+
 // IPv4: http://regexlib.com/REDetails.aspx?regexp_id=32
 var ipv4 = "(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$";
 // IPv6: http://regexlib.com/REDetails.aspx?regexp_id=906 + allow lowercase hexa letters (a-f)
@@ -21,6 +25,21 @@ function validateDomainName(domain)
 {
 	 jQuery.validator.addMethod(domain, function(value, element) {
 		var regExp = new RegExp("^"+fqdn+"$");
+      var ip4 = new RegExp(ipv4);
+      var ip6 = new RegExp(ipv6);
+      
+		return this.optional(element) 
+		  || regExp.test(value)
+        || ip4.test(value)
+        || ip6.test(value);
+	 });
+    //}, "Enter a valid domain name or an IP address.");
+}
+
+function validateDomainNameWithPortNumber(domain)
+{
+	 jQuery.validator.addMethod(domain, function(value, element) {
+		var regExp = new RegExp("^"+fqdn+port+"$");
       var ip4 = new RegExp(ipv4);
       var ip6 = new RegExp(ipv6);
       
