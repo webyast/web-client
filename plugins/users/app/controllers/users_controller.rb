e5453b7b2300320b3a58d8274598a09b0aca2cfc
Michal Zugec <mzugec@suse.cz>
2010-04-12 18:26:20 +0200

    Revert "users list (repository like) layout - not finished"
    This reverts commit 64ff3029adec70f4a7ad3947d2a47aa3e65019c7.

--- plugins/users/app/controllers/users_controller.rb
+++ plugins/users/app/controllers/users_controller.rb
@@ -24,17 +24,7 @@ class UsersController < ApplicationController
     @users = []
     begin
       @users = User.find :all
-      @users.each do |user|
-       user.user_password2 = user.user_password
-       user.uid_number	= user.uid_number
-       user.grouplist.each do |group|
-          if user.grp_string.blank?
-             user.grp_string = group.cn
-          else
-             user.grp_string += ",#{group.cn}"
-          end
-       end
-      end
+#      @groups = Group.find
       rescue ActiveResource::ClientError => e
         flash[:error] = YaST::ServiceResource.error(e)
     end
