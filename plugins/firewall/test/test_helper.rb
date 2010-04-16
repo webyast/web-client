260e9ed249c9a03b7345a063e7f2adbf8e2fea63
Klaus Kämpf <kkaempf@suse.de>
2010-04-16 11:31:53 +0200

    DRY test_helpers. All plugins use the parents one

--- plugins/firewall/test/test_helper.rb
+++ plugins/firewall/test/test_helper.rb
@@ -1,6 +1,2 @@
-ENV["RAILS_ENV"] = "test"
 require File.join(File.dirname(__FILE__), '..', 'config', 'rails_parent')
-require File.expand_path( File.join("config","environment"), RailsParent.parent )
-
-require 'active_support'
-require 'active_support/test_case'
+require File.join(RailsParent.parent, "test","test_helper")
