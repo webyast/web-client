f4cb8c51f6f4ac7c76cd3a2b2230859522465fd3
Martin Kudlvasr <mkudlvasr@novell.com>
2010-05-06 20:05:03 +0200

    licenses: fix bnc#581258 - eula tests do no cover all code

--- plugins/eulas/test/functional/eulas_controller_test.rb
+++ plugins/eulas/test/functional/eulas_controller_test.rb
@@ -30,11 +30,15 @@ class EulasControllerTest < ActionController::TestCase
   def setup
     # stub what the REST is supposed to return
     @eulas_response = fixture "eulas.xml"
+    @eulas_2_response = fixture "eulas2.xml"
+    @no_eulas_response = fixture "eulas_none.xml"
     @eulas_accepted_response = fixture "eulas_accepted.xml"
     @sles_eula_response = fixture "sles_eula.xml"
     @sles_eula_accepted_response = fixture "sles_eula_accepted.xml"
     @sles_eula_de_response = fixture "sles_eula_de.xml"
     @show_only_response = fixture "show_only_license.xml"
+    @opensuse_eula_response = fixture "opensuse_eula.xml"
+    @opensuse_eula_accepted_response = fixture "opensuse_eula_accepted.xml"
 
     EulasController.any_instance.stubs(:login_required)
     @controller = EulasController.new
@@ -112,5 +116,52 @@ class EulasControllerTest < ActionController::TestCase
     end
     post :update, :eula => {"accepted" => "true"}, "id" => "1"
     assert_redirected_to :controller => "controlpanel", :action => "nextstep"
+  end
+
+  def test_no_eulas
+    ActiveResource::HttpMock.respond_to do |mock|
+      header = ActiveResource::HttpMock.authentication_header
+      mock.resources :"org.opensuse.yast.modules.eulas" => "/eulas"
+      mock.permissions "org.opensuse.yast.modules.eulas", { :read => true, :write => true }
+      mock.get  "/eulas.xml", header, @no_eulas_response, 200
+    end
+    get :index
+    assert_response :success
+  end
+
+  def setup_2_eulas
+    ActiveResource::HttpMock.respond_to do |mock|
+      header = ActiveResource::HttpMock.authentication_header
+      mock.resources :"org.opensuse.yast.modules.eulas" => "/eulas"
+      mock.permissions "org.opensuse.yast.modules.eulas", { :read => true, :write => true }
+      mock.get  "/eulas.xml", header, @eulas_2_response, 200
+      mock.get  "/eulas/1.xml", header, @sles_eula_response, 200
+      mock.get  "/eulas/1.xml?lang=en_US", header, @sles_eula_response, 200
+      mock.get  "/eulas/1.xml?lang=en", header, @sles_eula_response, 200
+      mock.get  "/eulas/1.xml?lang=de", header, @sles_eula_de_response, 200
+      mock.get  "/eulas/2.xml", header, @opensuse_eula_response, 200
+      mock.get  "/eulas/2.xml?lang=en_US", header, @opensuse_eula_response, 200
+      mock.get  "/eulas/2.xml?lang=en", header, @opensuse_eula_response, 200
+    end
+  end
+
+  def test_2_eula_step
+    setup_2_eulas
+    get :index
+    assert_redirected_to "/eulas/show/1"
+    get :show, :id => 1
+    assert_response :success
+    post :update, :eula => {"accepted" => "false"}, "id" => "1"
+    assert_redirected_to "/eulas/show/1"
+    ActiveResource::HttpMock.respond_to do |mock|
+      header = ActiveResource::HttpMock.authentication_header
+      mock.resources :"org.opensuse.yast.modules.eulas" => "/eulas"
+      mock.permissions "org.opensuse.yast.modules.eulas", { :read => true, :write => true }
+      mock.get  "/eulas.xml", header, @eulas_accepted_response, 200
+      mock.get  "/eulas/1.xml", header, @sles_eula_response, 200
+      mock.put  "/eulas/1.xml", header, @sles_eula_accepted_response, 200
+    end
+    post :update, :eula => {"accepted" => "true"}, "id" => "1"
+    assert_redirected_to "/eulas/show/2"
   end
 end
