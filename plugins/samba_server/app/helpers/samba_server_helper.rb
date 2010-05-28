bd01ed0baa38923bd8725208b2500915ceaaa587
jreidinger <jreidinger@suse.cz>
2010-05-28 11:36:25 +0200

    remove obsolete samba server plugin

--- plugins/samba_server/app/helpers/samba_server_helper.rb
+++
@@ -1,8 +0,0 @@
-module SambaServerHelper
-
-    def isSMBtrue?(bool)
-	return false if bool.nil?
-	return (bool == true || bool.downcase == "yes" || bool.downcase == "true" || bool == "1" || bool == 1)
-    end
-
-end
