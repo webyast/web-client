836feb0cf613bbf3b53612bf5fbbc4daad624a9d
Martin Kudlvasr <mkudlvasr@novell.com>
2010-06-25 14:05:28 +0200

    webclient: make packaging work with sass

--- webclient/public/stylesheets/update_sass.sh
+++
@@ -1,3 +0,0 @@
-sass --update jquery-ui-1.7.2.custom-accordion.sass:jquery-ui-1.7.2.custom-accordion.css
-sass --update jquery-ui-1.7.2.custom-tabs.sass:jquery-ui-1.7.2.custom-tabs.css
-sass --update style.sass:style.css
