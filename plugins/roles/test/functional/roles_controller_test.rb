0c3446a27d591394b5ba5deb78dffa5fcc93789d
Josef Reidinger <jreidinger@suse.cz>
2010-07-01 13:49:47 +0200

    adapt testsuite to getent

--- plugins/roles/test/functional/roles_controller_test.rb
+++ plugins/roles/test/functional/roles_controller_test.rb
@@ -1,15 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
 
 class RolesControllerTest < ActionController::TestCase
-  def users_fixture
-    git_loc = File.join(File.dirname(__FILE__),"..","..","..","users","test","fixtures","users","users.xml")
-    if File.exists? git_loc
-      IO.read git_loc
-    else
-      IO.read File.join( RailsParent.parent, "vendor", "plugins", "users", "test", "fixtures", "users","users.xml" )
-    end
-  end
-
   def fixture(file)
     IO.read(File.join(File.dirname(__FILE__), "..", "fixtures", file))
   end
@@ -19,6 +10,7 @@ class RolesControllerTest < ActionController::TestCase
     response_roles = fixture "roles.xml"
     response_role = fixture "test2.xml"
     response_perm = fixture "permissions.xml"
+    response_users = fixture "getent.xml"
     ActiveResource::HttpMock.set_authentication
     ActiveResource::HttpMock.respond_to do |mock|
       header = ActiveResource::HttpMock.authentication_header
@@ -30,7 +22,7 @@ class RolesControllerTest < ActionController::TestCase
       mock.get  "/roles.xml", header, response_roles, 200
       mock.get  "/roles/test2.xml", header, response_role, 200
       mock.get  "/permissions.xml?with_description=1", ActiveResource::HttpMock.authentication_header_without_language, response_perm, 200
-      mock.get "/users.xml", header, users_fixture, 200
+      mock.get "/users.xml?getent=1", header, response_users, 200
     end
   end
 
