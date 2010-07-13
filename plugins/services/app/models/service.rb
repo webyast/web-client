09f73943e8aad2056fb579d5ff8b4f36b3975096
Jiri Suchomel <jsuchome@suse.cz>
2010-07-13 11:06:20 +0200

    - custom and enabled are now booleans, not strings (bnc#621822) - 0.2.0

--- plugins/services/app/models/service.rb
+++ plugins/services/app/models/service.rb
@@ -22,12 +22,4 @@
 class Service < ActiveResource::Base
   extend YastModel::Base
   model_interface :"org.opensuse.yast.modules.yapi.services"
-
-  def enabled?
-    return self.enabled == "true"
-  end
-
-  def custom?
-    return self.custom == "true"
-  end
 end
