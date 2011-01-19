cb86757ff0b7fe436d3d21386350674d2a473ba5
Stefan Schubert <schubi@suse.de>
2011-01-19 10:43:10 +0100

    checking for min javascripts

--- webclient/start.sh
+++ webclient/start.sh
@@ -2,4 +2,9 @@
 rm -f log/development.log
 rake db:migrate
 rake sass:update
+if [ -f public/javascripts/min/base-min.js ] ; then
+  echo "base-min.js already available"
+else
+  rake js:base
+fi
 ruby script/server --port=54984
