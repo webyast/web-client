7a6244f78432094333ff87490f74a64a0e4fb88c
Vladislav Gorobets <vgorobets@suse.de>
2011-04-12 12:31:54 +0200

    - added check for minified javascripts in users and status modules

--- webclient/start.sh
+++ webclient/start.sh
@@ -7,4 +7,20 @@ if [ -f public/javascripts/min/base-min.js ] ; then
 else
   rake js:base
 fi
+
+if [ -f ../plugins/users/public/javascripts/min/users-min.js ] ; then
+  echo "users-min.js already available"
+else
+  cd ../plugins/users/
+  rake js:users
+  cd -
+fi
+
+if [ -f ../plugins/status/public/javascripts/min/status-min.js ] ; then
+  echo "status-min.js already available"
+else
+  cd ../plugins/status/
+  rake js:status
+  cd -
+fi
 ruby script/server --port=54984
