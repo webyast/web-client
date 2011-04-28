025f57b46df39be4e470fbe04684c69eae1abb52
vlewin <vlewin@suse.de>
2011-04-28 13:58:18 +0200

    - adapted wait animation on submit - fixed: remove square brackets from smtp server text field

--- plugins/mail/app/controllers/mail_controller.rb
+++ plugins/mail/app/controllers/mail_controller.rb
@@ -1,20 +1,20 @@
 #--
 # Copyright (c) 2009-2010 Novell, Inc.
-# 
+#
 # All Rights Reserved.
-# 
+#
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of version 2 of the GNU General Public License
 # as published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, contact Novell, Inc.
-# 
+#
 # To contact Novell about this file by physical or electronic mail,
 # you may find current contact information at www.novell.com
 #++
@@ -37,6 +37,9 @@ class MailController < ApplicationController
     @mail.confirm_password	= @mail.password
     @mail.test_mail_address	= ""
     @mail.test_mail_address	= params["email"] if params.has_key? "email"
+
+    #remove square brackets from smtp_server string
+    @mail.smtp_server = @mail.smtp_server.gsub(/^(\[)|(\])/, '')
   end
 
   # PUT
@@ -49,7 +52,7 @@ class MailController < ApplicationController
     if @mail.password != @mail.confirm_password
       flash[:error] = _("Passwords do not match.")
       redirect_to :action => "index"
-      return 
+      return
     end
 
     begin
