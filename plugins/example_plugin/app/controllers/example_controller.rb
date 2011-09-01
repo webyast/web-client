3227d9928095b2f0eb18bb97b4268156e947cd30
Stefan Schubert <schubi@suse.de>
2011-09-01 12:37:33 +0200

    saving text

--- plugins/example_plugin/app/controllers/example_controller.rb
+++ plugins/example_plugin/app/controllers/example_controller.rb
@@ -2,20 +2,32 @@ require 'yast/service_resource'
 require 'example'
 
 class ExampleController < ApplicationController
-  before_filter :login_required, :prepare
+  before_filter :login_required, :read_permissions
 
- private
+private
 
-  def prepare
+  def read_permissions
     @permissions = Example.permissions
   end
 
 public
 
   # GET /example
-  # GET /example.xml
   def index
     @example = Example.find(:one)
+  end
+
+  # PUT /example
+  def update
+    @example = Example.find(:one)
+    @example.content = params["example"]["content"]
+    begin
+      @example.save
+      flash[:notice] = _("File saved")
+    rescue Exception => error
+      flash[:error] = _("Error while saving file: %s") % error.inspect
+    end
+    render :index
   end
 
 end
